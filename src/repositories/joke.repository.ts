e5b44580cd69368031e6a9b5e8a7e800304fc6d2
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 00:33:37 +0530

    feat: Add Joke repository for CRUD operations on jokes

---
+++ src/repositories/joke.repository.ts
@@ -0,0 +1,24 @@
+import Joke from '../models/joke.model';
+
+const getJokes = async () => {
+  return await Joke.find().populate('type', 'name');
+};
+
+const createJoke = async (data: { setup: string; punchline: string; type: string; author: string }) => {
+  const joke = new Joke(data);
+  return await joke.save();
+};
+
+const getJokeById = async (id: string) => {
+  return await Joke.findById(id).populate('type', 'name');
+};
+
+const updateJoke = async (id: string, data: Partial<{ setup: string; punchline: string; type: string; author: string }>) => {
+  return await Joke.findByIdAndUpdate(id, data, { new: true });
+};
+
+const deleteJoke = async (id: string) => {
+  return await Joke.findByIdAndDelete(id);
+};
+
+export { getJokes, createJoke, getJokeById, updateJoke, deleteJoke };
