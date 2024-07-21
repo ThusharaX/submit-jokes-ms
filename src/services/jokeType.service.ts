da2e15ecaf307de6c73f1e4ee2e39a55f948ebb2
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 21:09:44 +0530

    refactor: Update jokeType.service.ts to use production domain for fetching joke types

--- src/services/jokeType.service.ts
+++ src/services/jokeType.service.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { envs } from '../config/env';
 
 const getJokeTypesService = async () => {
-  const url = 'http://localhost:3003/joke-types';
+  const url = `${envs.PROD_DOMAIN}/deliver/joke-types`;
   const response = await axios.get(url);
   return response.data;
 };
